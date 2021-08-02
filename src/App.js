20a5f0e80f2548ca3e8d43e90ab035371c4edd79
Ahmad Raza Khan <raza82504@gmail.com>
2021-08-02 13:20:04 +0530

    just added firebase.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import firebase from "./firebase/firebase";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    console.log(firebase);
     return (
       <div>
         <h1>This is woohoo created new things </h1>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDi6-hnBrCXaE2dSG_9BSeuTReDxuUoxUA",
+  authDomain: "mubaraa-edaf4.firebaseapp.com",
+  projectId: "mubaraa-edaf4",
+  storageBucket: "mubaraa-edaf4.appspot.com",
+  messagingSenderId: "238598170864",
+  appId: "1:238598170864:web:a7989ecc67b67d95313a35",
+  measurementId: "G-PY3VDWJE71"
+})
+
+export default firebase;
