48e91a9e52876f136e86b7296a9047351806d1ff
charles930030 <raza82504@gmail.com>
2021-08-02 11:46:55 +0530

    creating my repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is woohoo</h1>
+      </div>
+    );
+  }
+}
+export default App;
